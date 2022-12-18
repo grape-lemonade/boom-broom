df18b2f7a3c9319cd1fe7ac5b0507d6ae5121975
Taylor Hiatt <taylor@hiatt.dev>
2022-12-17 20:14:21 -0500

    not really sure what i did here

--- src/tile.rs
+++ src/tile.rs
@@ -1,13 +1,39 @@
+use std::{borrow::Borrow, collections::HashMap, sync::Mutex};
+
+use once_cell::sync::Lazy;
 use rstar::{RTreeObject, AABB};
-use sfml::graphics::{Drawable, Image, Texture, Rect};
+use sfml::{graphics::{Drawable, Image, Texture, Rect, Sprite}, SfBox};
+
+const Textures: Lazy<Mutex<HashMap<&str, SfBox<Texture>>>> = Lazy::new(|| {
+    let mut m = HashMap::new();
+
+    m.insert("tile_hidden", Texture::from_file("./data/images/tile_hidden.png").unwrap());
+    Mutex::new(m)
+});
 
 #[derive(Debug)]
 pub struct Tile {
     state: TileState,
     pos: (i32, i32), //0 is x, 1 is y
     tile_type: TileType,
-    img: Image,
+}
 
+impl Drawable for Tile {
+    fn draw<'a: 'shader, 'texture, 'shader, 'shader_texture>(
+            &'a self,
+            target: &mut dyn sfml::graphics::RenderTarget,
+            states: &sfml::graphics::RenderStates<'texture, 'shader, 'shader_texture>,
+        ) {
+            //todo
+        match self.get_state() {
+            TileState::REVEALED => {
+                
+            },
+            TileState::HIDDEN => todo!(),
+            TileState::FLAGGED => todo!(),
+            TileState::EXPLODED => todo!(),
+        }
+    }
 }
 
 impl RTreeObject for Tile {
@@ -18,23 +44,14 @@ impl RTreeObject for Tile {
     }
 }
 
-impl Drawable for Tile {
-    fn draw<'a: 'shader, 'texture, 'shader, 'shader_texture>(
-        &'a self,
-        target: &mut dyn sfml::graphics::RenderTarget,
-        states: &sfml::graphics::RenderStates<'texture, 'shader, 'shader_texture>,
-    ) {
-        &self.draw();
-    }
-}
-
 impl Tile {
     pub fn new(ipos: (i32, i32), sprite: &str) -> Tile {
+        let mut tex = Texture::new().expect("error allocating tile texture");
+        tex.load_from_file(sprite, Rect::new(0, 0, 32, 32));
         Tile {
             state: TileState::HIDDEN,
             pos: ipos,
             tile_type: TileType::CLEAR,
-            img: Image::from_file("./data/images/tile_hidden.png").unwrap(),
         }
     }
 
@@ -68,9 +85,6 @@ impl Tile {
         todo!();
     }
 
-    pub fn draw(&self) {
-        let mut tex = Texture::new().unwrap();
-    }
 }
 #[derive(Debug)]
 pub enum TileState {
