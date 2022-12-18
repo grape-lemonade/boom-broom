6637e7d4b671197fc4401a3f84fe139f723a8fd7
Taylor Hiatt <taylor@hiatt.dev>
2022-12-17 21:09:39 -0500

    switch to sdl2 for rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
-mod gamestate;
-mod tile;
-mod toolbox;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
 
-use sfml::window::{self as sf};
-use sf::{Window, Event, Style};
-use crate::gamestate::*;
-use crate::tile::{Tile, TileState};
-use crate::toolbox::{EventCode, Toolbox};
-use once_cell::sync::{Lazy, OnceCell};
-use std::sync::mpsc;
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-fn launch() {
-    // Launches the game, initializing the window and loading the sprites,
-    let mut TOOLBOX = Toolbox::new();
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
 
-    TOOLBOX.window.set_framerate_limit(60);
+    let mut canvas = window.into_canvas().build().unwrap();
 
-    while TOOLBOX.window.is_open() {
-        TOOLBOX.window.set_active(true);
-        TOOLBOX = TOOLBOX.GameLoop();
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
-}
-
-
-
-fn main() {
-    println!("Initializing...");
-    launch();
 }
